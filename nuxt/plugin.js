9df3ab3fedcb4dd2b66301b3caa37de6c8aabc55
Chahin <chahin2002@outlook.com>
2025-01-10 01:06:14 +0100

    Clean up

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from '#app'
-import MasonryWall from '../src/components/MasonryWall.vue' // Adjust the path based on your project structure
+import MasonryWall from '../src/components/MasonryWall.vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('MasonryWall', MasonryWall)
--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,4 +1,4 @@
-import { computed } from 'vue';
+import { computed } from 'vue'
 
 // Imported from https://github.com/DerYeger/yeger/tree/main/packages/vue-lib-adapter/src/index.ts
 
@@ -8,10 +8,7 @@ export interface VueRef<T> {
 
 export type VueVersion = 2 | 3
 
-export type Watch = (
-  refs: (VueRef<unknown> | undefined)[],
-  callback: () => void,
-) => void
+export type Watch = (refs: (VueRef<unknown> | undefined)[], callback: () => void) => void
 
 export type LifecycleHook = (callback: () => void) => void
 
@@ -25,17 +22,14 @@ export type LifecycleHook = (callback: () => void) => void
  * @param delay - A delay after which the callback will be invoked.
  * @returns The debounced callback.
  */
-export function debounce<Args extends any[]>(
-  cb: (...args: Args) => void,
-  delay?: number
-) {
-  let timeout: any;
+export function debounce<Args extends any[]>(cb: (...args: Args) => void, delay?: number) {
+  let timeout: any
   return (...args: Args) => {
     if (timeout !== undefined) {
-      clearTimeout(timeout);
+      clearTimeout(timeout)
     }
-    timeout = setTimeout(() => cb(...args), delay);
-  };
+    timeout = setTimeout(() => cb(...args), delay)
+  }
 }
 
 // -------------------------------------------------------------------------------------------------------
@@ -93,31 +87,24 @@ export function useMasonryWall<T>({
   ssrColumns,
   vue,
   wall,
-  watch
+  watch,
 }: HookProps<T>) {
   function countIteratively(
     containerWidth: number,
     gap: number,
     count: number,
-    consumed: number
+    consumed: number,
   ): number {
-    const nextWidth = getColumnWidthTarget(count);
+    const nextWidth = getColumnWidthTarget(count)
     if (consumed + gap + nextWidth <= containerWidth) {
-      return countIteratively(
-        containerWidth,
-        gap,
-        count + 1,
-        consumed + gap + nextWidth
-      );
+      return countIteratively(containerWidth, gap, count + 1, consumed + gap + nextWidth)
     }
-    return count;
+    return count
   }
 
   function getColumnWidthTarget(columnIndex: number): number {
-    const widths = Array.isArray(columnWidth.value)
-      ? columnWidth.value
-      : [columnWidth.value];
-    return widths[columnIndex % widths.length] as number;
+    const widths = Array.isArray(columnWidth.value) ? columnWidth.value : [columnWidth.value]
+    return widths[columnIndex % widths.length] as number
   }
 
   function columnCount(): number {
@@ -126,122 +113,114 @@ export function useMasonryWall<T>({
       gap.value,
       0,
       // Needs to be offset my negative gap to prevent gap counts being off by one
-      -gap.value
-    );
-    const boundedCount = aboveMin(belowMax(count));
-    return boundedCount > 0 ? boundedCount : 1;
+      -gap.value,
+    )
+    const boundedCount = aboveMin(belowMax(count))
+    return boundedCount > 0 ? boundedCount : 1
   }
 
   function belowMax(count: number): number {
-    const max = maxColumns?.value;
+    const max = maxColumns?.value
     if (!max) {
-      return count;
+      return count
     }
-    return count > max ? max : count;
+    return count > max ? max : count
   }
 
   function aboveMin(count: number): number {
-    const min = minColumns?.value;
+    const min = minColumns?.value
     if (!min) {
-      return count;
+      return count
     }
-    return count < min ? min : count;
+    return count < min ? min : count
   }
 
-  if (ssrColumns.value > 0) {
-    const newColumns = createColumns(ssrColumns.value);
-    items.value.forEach((_: T, i: number) =>
-      newColumns[i % ssrColumns.value]!.push(i)
-    );
-    columns.value = newColumns;
+  if (ssrColumns.value > 0 && items.value?.length > 0) {
+    const newColumns = createColumns(ssrColumns.value)
+    items.value.forEach((_: T, i: number) => newColumns[i % ssrColumns.value]!.push(i))
+    columns.value = newColumns
   }
 
-  let currentRedrawId = 0;
+  let currentRedrawId = 0
 
   async function fillColumns(itemIndex: number, assignedRedrawId: number) {
-    if (itemIndex >= items.value.length) {
-      return;
+    if (itemIndex >= items.value?.length || items.value === undefined) {
+      return
     }
-    await nextTick();
+    await nextTick()
     if (currentRedrawId !== assignedRedrawId) {
       // Skip if a new redraw has been requested in parallel,
       // e.g., in an onMounted hook during initial render
-      return;
+      return
     }
-    const columnDivs = [...wall.value.children] as HTMLDivElement[];
+    const columnDivs = [...wall.value.children] as HTMLDivElement[]
     if (rtl.value) {
-      columnDivs.reverse();
+      columnDivs.reverse()
     }
     const target = columnDivs.reduce((prev, curr) =>
-      curr.getBoundingClientRect().height < prev.getBoundingClientRect().height
-        ? curr
-        : prev
-    );
-    columns.value[+target.dataset.index!]!.push(itemIndex);
-    await fillColumns(itemIndex + 1, assignedRedrawId);
+      curr.getBoundingClientRect().height < prev.getBoundingClientRect().height ? curr : prev,
+    )
+    columns.value[+target.dataset.index!]!.push(itemIndex)
+    await fillColumns(itemIndex + 1, assignedRedrawId)
   }
 
   async function redraw(force = false) {
-    const newColumnCount = columnCount();
+    const newColumnCount = columnCount()
     if (columns.value.length === newColumnCount && !force) {
       if (vue === 2) {
-        ;(emit as Vue2ComponentEmits)('redraw-skip');
+        ;(emit as Vue2ComponentEmits)('redraw-skip')
       } else {
-        ;(emit as Vue3ComponentEmits)('redrawSkip');
+        ;(emit as Vue3ComponentEmits)('redrawSkip')
       }
-      return;
+      return
     }
-    columns.value = createColumns(newColumnCount);
-    const scrollTarget = scrollContainer?.value;
-    const scrollY = scrollTarget ? scrollTarget.scrollTop : window.scrollY;
+    columns.value = createColumns(newColumnCount)
+    const scrollTarget = scrollContainer?.value
+    const scrollY = scrollTarget ? scrollTarget.scrollTop : window.scrollY
 
-    await fillColumns(0, ++currentRedrawId);
+    await fillColumns(0, ++currentRedrawId)
     if (scrollTarget && scrollTarget instanceof HTMLElement) {
-      scrollTarget?.scrollBy({ top: scrollY - scrollTarget.scrollTop });
+      scrollTarget?.scrollBy({ top: scrollY - scrollTarget.scrollTop })
     }
-    emit('redraw');
+    emit('redraw')
   }
 
   const resizeObserver =
-    typeof ResizeObserver === 'undefined'
-      ? undefined
-      : new ResizeObserver(debounce(() => redraw()));
+    typeof ResizeObserver === 'undefined' ? undefined : new ResizeObserver(debounce(() => redraw()))
 
   onMounted(() => {
-    redraw();
-    resizeObserver?.observe(wall.value);
-  });
+    redraw()
+    resizeObserver?.observe(wall.value)
+  })
 
-  onBeforeUnmount(() => resizeObserver?.unobserve(wall.value));
+  onBeforeUnmount(() => resizeObserver?.unobserve(wall.value))
 
-  watch([items, rtl], () => redraw(true));
-  watch([columnWidth, gap, minColumns, maxColumns], () => redraw());
+  watch([items, rtl], () => redraw(true))
+  watch([columnWidth, gap, minColumns, maxColumns], () => redraw())
 
-  const hadMultipleRows = computed(() =>
-    columns.value.some((column) => column.length > 1)
-  );
+  const hadMultipleRows = computed(() => columns.value.some((column) => column.length > 1))
 
   const filteredColumns = computed(() =>
     only1Row.value
       ? columns.value.map((column) => (column.length > 0 ? [column[0]] : []))
-      : columns.value
-  );
+      : columns.value,
+  )
 
   const remainingItems = computed(() => {
     if (!only1Row.value) {
-      return 0;
+      return 0
     }
 
-    return items.value.length - filteredColumns.value?.length
+    return items.value?.length ?? 0 - filteredColumns.value?.length
   })
 
   function isLastColumn(columnIndex: number) {
-    return columnIndex === (only1Row.value ? filteredColumns.value : columns.value).length - 1;
+    return columnIndex === (only1Row.value ? filteredColumns.value : columns.value).length - 1
   }
 
-  return { hadMultipleRows, filteredColumns, remainingItems, isLastColumn, getColumnWidthTarget };
+  return { hadMultipleRows, filteredColumns, remainingItems, isLastColumn, getColumnWidthTarget }
 }
 
 function createColumns(count: number): Column[] {
-  return Array.from({ length: count }).map(() => []);
+  return Array.from({ length: count }).map(() => [])
 }
